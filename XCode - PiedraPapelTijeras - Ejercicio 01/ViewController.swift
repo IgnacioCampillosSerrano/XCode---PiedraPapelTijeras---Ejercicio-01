c9b86a83fd98569575f7c7405bf48f6df339543b
ignaciocampillosserrano <ignacio.campillos.serrano@cieep.com>
2023-10-05 14:13:50 +0200

    Initial Commit

---
+++ XCode - PiedraPapelTijeras - Ejercicio 01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCode - PiedraPapelTijeras - Ejercicio 01
+//
+//  Created by Ignacio Campillos Serrano on 05/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XCode - PiedraPapelTijeras - Ejercicio 01Tests/XCode___PiedraPapelTijeras___Ejercicio_01Tests.swift
@@ -0,0 +1,33 @@
+//
+//  XCode___PiedraPapelTijeras___Ejercicio_01Tests.swift
+//  XCode - PiedraPapelTijeras - Ejercicio 01Tests
+//
+//  Created by Ignacio Campillos Serrano on 05/10/2023.
+//
+
+import XCTest
+@testable import XCode___PiedraPapelTijeras___Ejercicio_01
+
+class XCode___PiedraPapelTijeras___Ejercicio_01Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
